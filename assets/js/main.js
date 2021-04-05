30377eb41830893f9c21d16d5b6ef3869cab0e7a
Leon <leonwheeler08@gmail.com>
2021-04-05 14:10:53 +0100

    Refactor some lines with jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,7 @@ const renderForecastCardComponent = (forecastData) => {
 
 const fetchAllWeatherData = (requestedCityName) => {
   // construct URL for http://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY} and store in variable called as weatherApiUrl
-  let cityName = document.getElementById(`form1`).value;
+  let cityName = $(`#form1`).val();
   const weatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
   const functionForJSON = (responseObject) => {
     // unless you have some logic here do that before you return
@@ -93,14 +93,13 @@ const onLoad = () => {
 // function called when the form is submitted
 const onSubmit = () => {
   // get city name and store in variable called cityName
-  let cityName = document.getElementById(`form1`).value;
+  let cityName = $(`#form1`).val();
   // fetchAllWeatherData(cityName)
 };
 
 const onClick = () => {
-  let cityName = document.getElementById(`form1`).value;
+  let cityName = $(`#form1`).val();
   console.log(cityName);
-  fetchAllWeatherData();
   // get city name from the list item that was clicked and store in variable called cityName
   // fetchAllWeatherData(cityName)
 };
