7c80ee0261b6c77b1ffac24cc080f0e7ca557389
Leon <leonwheeler08@gmail.com>
2021-04-07 18:05:55 +0100

    Recent city is now stored in local storage. Need to allow 5 to be stored.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
 const API_KEY = "a16cb7d329dca1d63027fe78612143ab";
 let requestedData;
 
-const storeRecentCity = () => {};
+const storedCityData = () => {
+  let cityName = $(`#form1`).val();
+  let storedCity = JSON.parse(localStorage.getItem(`${cityName}`) || "[]");
+  // for (let i = 0; i < 5; i++) {
+  const pushToArray = () => {
+    let cityName = $(`#form1`).val();
+    storedCity.push(cityName);
+  };
+  const saveCity = () => {
+    localStorage.setItem(`recentCity`, storedCity);
+  };
+  pushToArray();
+  saveCity();
+  // }
+};
 
 const renderCurrentWeather = (currentWeather) => {
   let cityName = $(`#form1`).val();
@@ -52,6 +66,7 @@ async function fetchAllWeatherData() {
 const onClick = () => {
   fetchAllWeatherData();
   let cityName = $(`#form1`).val();
+  storedCityData();
   // get city name from the list item that was clicked and store in variable called cityName
   // fetchAllWeatherData(cityName)
 };
