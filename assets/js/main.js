4b253dce0697d2ec80be0da40e4f8b4467b723d6
Leon <leonwheeler08@gmail.com>
2021-04-09 22:39:13 +0100

    UV index is now also given when searching for a city.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const API_KEY = "a16cb7d329dca1d63027fe78612143ab";
 let requestedData;
-let oneCallData;
+let oneCallRequestedData;
 let storedCity = JSON.parse(localStorage.getItem(`recentCities`) || "[]");
 $(document).ready(function () {
   renderRecentCities();
@@ -30,7 +30,7 @@ ${storedCity[i]}
   }
 };
 
-const renderCurrentWeather = (currentWeather) => {
+const renderCurrentWeather = (currentWeather, currentOneCall) => {
   let cityName = $(`#form1`).val();
   console.log(cityName);
   $("#renderedCityName").replaceWith(
@@ -40,6 +40,7 @@ const renderCurrentWeather = (currentWeather) => {
   $("#temp").text(`Temperature: ${currentWeather.temperature}°C`);
   $("#humidity").text(`Humidity: ${currentWeather.humidity}%`);
   $("#windSpeed").text(`Wind Speed: ${currentWeather.windSpeed} mph`);
+  $("#uvIndex").text(`UV Index: ${currentOneCall.uvi}`);
   console.log(currentWeather.name);
 };
 
@@ -86,10 +87,12 @@ async function fetchAllWeatherData() {
   const oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${requestedData.lat}&lon=${requestedData.lon}&exclude=hourly,minutely&appid=${API_KEY}`;
   const oneCallResponse = await fetch(oneCallApiUrl);
   const oneCallData = await oneCallResponse.json();
-
+  oneCallRequestedData = {
+    uvi: oneCallData.current.uvi,
+  };
   console.log(oneCallData);
   console.log(requestedData);
-  renderCurrentWeather(requestedData);
+  renderCurrentWeather(requestedData, oneCallRequestedData);
 }
 
 const onClick = () => {
